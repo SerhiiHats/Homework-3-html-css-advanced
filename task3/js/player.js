df5977daf7c92ebcd30e84f0fc57a0e1d431ffe3
Serhi Hats <gatsserv@gmail.com>
2023-04-08 18:00:28 +0300

    add all button in player

--- task3/js/player.js
+++ task3/js/player.js
@@ -1,5 +1,6 @@
 let video = document.querySelector("#video");
 let time = document.querySelector("#time");
+let iconPlay = document.querySelector("#play>img");
 let timeId;
 
 function getParseTime(date) {
@@ -10,7 +11,6 @@ function getParseTime(date) {
 
 function viewTime() {
     let milSecCurrent = video.currentTime * 1000;
-    // new Date().getSeconds
     let carrentTime = getParseTime(new Date(milSecCurrent));
     let milSecTotalTime = video.duration * 1000;
     let totalTime = getParseTime(new Date(milSecTotalTime));
@@ -23,16 +23,46 @@ document.querySelector("#play").onclick = (event) => {
         event.target.src = "icon/pause-solid.svg";
         video.play();
         timeId = setInterval(() => { viewTime() }, 1000);
-console.log( timeId)
+        console.log(timeId)
         console.log(video.currentTime)
         console.log(video.duration)
     } else {
         event.target.src = "icon/play-solid.svg";
-       
-        video.pause(); 
+
+        video.pause();
         console.log("before", timeId)
         clearInterval(timeId);
         console.log("after", timeId)
         console.log(video.currentTime)
     }
+}
+
+document.querySelector("#backward").onclick = () => {
+    video.currentTime -= 10;
+    viewTime();
+}
+
+document.querySelector("#forward").onclick = () => {
+    video.currentTime += 10;
+    viewTime();
+}
+
+document.querySelector("#stop").onclick = (event) => {
+    iconPlay.src = "icon/play-solid.svg";
+    video.currentTime = 0;
+    viewTime();
+    video.pause();
+}
+
+
+document.querySelector("#volume-low").onclick = () => {
+    video.volume -= 0.1;
+}
+
+document.querySelector("#volume-high").onclick = () => {
+    video.volume += 0.1;
+}
+
+document.querySelector("#volume-stop").onclick = () => {
+    video.volume = 0;
 }
